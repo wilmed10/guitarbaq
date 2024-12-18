cef7945931b8d05c7adca960b10d745e4590c3fc
Wilson Medina <wilsonmedina8d@gmail.com>
2024-12-18 16:50:57 -0500

    update img path

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -7,7 +7,7 @@ export default function Guitar({guitar, addToCart}) {
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                   <div className="col-4">
-                      <img className="img-fluid" src={`/img/${ image }.jpg`} alt="imagen guitarra" />
+                      <img className="img-fluid" src={`../img/${ image }.jpg`} alt="imagen guitarra" />
                   </div>
                   <div className="col-8">
                       <h3 className="text-black fs-4 fw-bold text-uppercase">{ name }</h3>
